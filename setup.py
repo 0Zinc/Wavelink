ffda5ce2686f08d82fd4f38a00bb871f50d985c5
ZingyAwesome <zingy@zingyawesome.com>
2021-12-12 17:23:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if on_rtd:
     with open("docs/requirements.txt") as f:
         requirements.extend(f.read().splitlines())
 
-version = "{{__VERSION__}}"
+version = "1.0.0b"
 
 readme = ""
 with open("README.rst") as f:
